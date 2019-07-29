317074594899c3faac80f26ec8312a312e993e7d
caolitao <1657227541@qq.com>
2019-07-29 21:08:03 +0800

    重新提交

---
+++ exercise/chap01/TestMain.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TestMain{
+	public static void main(String[] args){
+		args = new String[3];
+		args[0] = new Scanner(System.in).next();
+		System.out.println(args[0]);
+	}
+
+
+}
