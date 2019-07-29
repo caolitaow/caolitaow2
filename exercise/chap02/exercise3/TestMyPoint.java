ca2b51088a0b3a10d500996c3de8ae50f99a27a4
clt <1657227541@qq.com>
2019-07-29 19:37:46 +0800

    分支的基本操作

--- exercise/chap02/exercise3/TestMyPoint.java
+++ exercise/chap02/exercise3/TestMyPoint.java
@@ -4,19 +4,12 @@ public class TestMyPoint{
       MyPoint start=new MyPoint(10,10);
 	  MyPoint end=new MyPoint(20,30);
 	  System.out.println("start:"+start+'\n'+"end:"+end);
-	  MyPoint stray=new MyPoint();
-      stray=end;
-	  System.out.println("end:"+end+'\n'+"stray:"+stray);
-	  stray.setX(47);
-	  stray.setY(50);
-	  
-	  System.out.println("start:"+start+'\n'+"end:"+end+'\n'+"stray:"+stray);
-     MyPoint stray1;
-	 stray1=end;
-	 stray1.setX(07);
-     stray1.setY(07);
+	  MyPoint stray=end;
 
-	  System.out.println("start:"+start+'\n'+"end:"+end+'\n'+"stray:"+stray1);
+	  System.out.println("end:"+end+'\n'+"stray:"+stray);
+	  stray.MyPoint(47,50);
+	  System.out.println("start:"+start+'\n'+"end:"+end+'\n'+"stray:"+stray);
+     
    }
 
 
